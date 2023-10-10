2d780a4361bb48fd93d37e4f64915418bd81536d
Omolola Oladeinde <aderonke11@gmail.com>
2023-10-10 09:12:40 +0100

    add error handling for city

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function formatDate() {
   let minutes = now.getMinutes();
 
   if (hours < 10) {
-    hours = `0${hours}`
+    hours = `0${hours}`;
   }
   if (minutes < 10) {
     minutes = `0${minutes}`;
@@ -27,9 +27,9 @@ function formatDate() {
 
 formatDate();
 
-let unit=document.querySelector("#submit-form");
+let unit = document.querySelector("#submit-form");
 
-function displaySearch(event){
+function displaySearch(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input");
   //city.innerHTML = response.data.name;
@@ -49,45 +49,48 @@ function displaySearch(event){
     console.log(response.data);
     //console.log(response.data.main.temp);
     // change temperature by city
-    let temp = Math.round(response.data.main.temp);
-    let newWeather = document.querySelector(".temp");
-    newWeather.innerHTML = `${temp}`;
-    // change description by city
-    let descrip= response.data.weather[0].description;
-    let state= document.querySelector("#weather-state");
-    state.innerHTML=`${descrip}`;
-    // change humidity by city
-    let humid=document.querySelector("#hum");
-    let humValue=response.data.main.humidity;
-    humid.innerHTML=`Humidity: ${humValue}%`;
-    // change wind speed by city
-    let wind=document.querySelector("#win");
-    let windValue=Math.round(response.data.wind.speed);
-    wind.innerHTML=`Wind: ${windValue} km/h`;
-    // change visibility
-    let visibility=document.querySelector("#vis");
-    let visiValue=Math.round(response.data.main.feels_like);
-    visibility.innerHTML = `Feels like ${visiValue}°C`;
+    if (response.data.cod === "404") {
+      // City not found
+      alert("City not found. Please enter a valid city name.");
+    } else {
+      // Display weather data
+      // Your existing code for displaying weather
+      let temp = Math.round(response.data.main.temp);
+      let newWeather = document.querySelector(".temp");
+      newWeather.innerHTML = `${temp}`;
+      // change description by city
+      let descrip = response.data.weather[0].description;
+      let state = document.querySelector("#weather-state");
+      state.innerHTML = `${descrip}`;
+      // change humidity by city
+      let humid = document.querySelector("#hum");
+      let humValue = response.data.main.humidity;
+      humid.innerHTML = `Humidity: ${humValue}%`;
+      // change wind speed by city
+      let wind = document.querySelector("#win");
+      let windValue = Math.round(response.data.wind.speed);
+      wind.innerHTML = `Wind: ${windValue} km/h`;
+      // change visibility
+      let visibility = document.querySelector("#vis");
+      let visiValue = Math.round(response.data.main.feels_like);
+      visibility.innerHTML = `Feels like ${visiValue}°C`;
+    }
   }
 
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city2}&appid=${apiKey}&units=metric`;
   axios.get(url).then(getWeather);
-
-
 }
 
 unit.addEventListener("submit", displaySearch);
 let search = document.querySelector("#search");
 search.addEventListener("click", displaySearch);
 
-
-function displayLocation(){
+function displayLocation() {
   function getTemp(response) {
-
-    let geoCity=response.data.name;
+    let geoCity = response.data.name;
     console.log(response.data);
-    let city=document.querySelector("#nameCity");
-    city.innerHTML=`${geoCity}`
+    let city = document.querySelector("#nameCity");
+    city.innerHTML = `${geoCity}`;
     let temp = document.querySelector(".temp");
     let temperature = Math.round(response.data.main.temp);
     temp.innerHTML = `${temperature} `;
@@ -116,14 +119,7 @@ function displayLocation(){
     axios.get(url).then(getTemp);
   }
   navigator.geolocation.getCurrentPosition(getPosition);
-
 }
 
-let current=document.querySelector("#current");
-current.addEventListener("click",displayLocation)
-
-
-
-
-
-
+let current = document.querySelector("#current");
+current.addEventListener("click", displayLocation);
